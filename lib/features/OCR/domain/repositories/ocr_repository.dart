19636edc0b2344636cef64c00479e55f35e342bf
EANimesha <nimeshadilini999@gmail.com>
2019-12-22 23:31:24 +0530

    write the first test for the usecase of converting image to text

--- lib/features/OCR/domain/repositories/ocr_repository.dart
+++ lib/features/OCR/domain/repositories/ocr_repository.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:dartz/dartz.dart';
 import 'package:ocr_mobile/core/error/failures.dart';
-import 'package:ocr_mobile/features/OCR_ImageToText/domain/entities/ocr_result.dart';
+import 'package:ocr_mobile/features/OCR/domain/entities/ocr_result.dart';
 
 abstract class OcrRepository{
   Future<Either<Failure,OcrResult>> convertImageToText(File image);
--- lib/features/OCR/domain/usecases/convert_image_to_text.dart
+++ lib/features/OCR/domain/usecases/convert_image_to_text.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:ocr_mobile/core/error/failures.dart';
-import 'package:ocr_mobile/features/OCR_ImageToText/domain/entities/ocr_result.dart';
-import 'package:ocr_mobile/features/OCR_ImageToText/domain/repositories/ocr_repository.dart';
+import 'package:ocr_mobile/features/OCR/domain/entities/ocr_result.dart';
+import 'package:ocr_mobile/features/OCR/domain/repositories/ocr_repository.dart';
 
 class ConvertImageToText{
   final OcrRepository repository;
